734989e8099623345402806c6c90893d4e8aef84
yoo hyun seog <dbghwns3@gmail.com>
2017-03-14 08:02:32 +0900

    test

--- naversearch_api/index.php
+++ naversearch_api/index.php
@@ -2,7 +2,7 @@
 $client_id = "8Is5YrAO6QkzBSXiwPSV";
 $client_secret = "2TbpRTesu8";
 $encText = urlencode("네이버오픈API");
-$url = "https://openapi.naver.com/v1/search/blog.xml?query=".$encText; // json 결과
+$url = "https://openapi.naver.com/v1/search/movie.xml?query=".$encText; // json 결과
 //  $url = "https://openapi.naver.com/v1/search/blog.xml?query=".$encText; // xml 결과
 $is_post = false;
 $ch = curl_init();
@@ -12,7 +12,7 @@ curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 $headers = array();
 $headers[] = "X-Naver-Client-Id: ".$client_id;
 $headers[] = "X-Naver-Client-Secret: ".$client_secret;
-curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);                            
 $response = curl_exec ($ch);
 $status_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
 echo "status_code:".$status_code."
